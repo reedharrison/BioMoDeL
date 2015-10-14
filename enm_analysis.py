19329ebca97b6546052e537691aca3ec15852259
reedharrison <rharr009@ucr.edu>
2015-10-13 17:13:12 -0700

    Initial enm analysis complete
    Script can now calculate elastic landscape and calculate hookean
    energies

--- enm_analysis.py
+++ enm_analysis.py
@@ -6,7 +6,7 @@
 def calcElasticEnergy(pdb_ref, ensemble, sel, cutoff, k=1):
 	import prody as pd
 	import numpy as np
-	import scipy as sci
+	from scipy import spatial
 
 	# Select desired atoms, retrieve reference parameters, and initialize Kirchhoff matrix
 	bb_ref = pdb_ref.select(sel)
@@ -16,17 +16,17 @@ def calcElasticEnergy(pdb_ref, ensemble, sel, cutoff, k=1):
 	kirchhoff = gnm.getKirchhoff()
 	np.fill_diagonal(kirchhoff, 0)
 	kirchhoff = abs(kirchhoff)
-	kirchhoff = sci.spatial.distance.squareform(kirchhoff)
+	kirchhoff = spatial.distance.squareform(kirchhoff)
 
 	# Calculate distances between Kirchhoff contacts
-	eq_dists = sci.spatial.distance.pdist(coords_ref)
+	eq_dists = spatial.distance.pdist(coords_ref)
 
 	# Iterate over coords within ensemble
 	energies = np.zeros(len(ensemble))
 	ind = 0
 	for coords in ensemble.iterCoordsets():
 		print('Calculating elastic energies for element %d\n' % ind)
-		dists = sci.spatial.distance.pdist(coords)
+		dists = spatial.distance.pdist(coords)
 		energies[ind] = f_hookean(eq_dists, dists, k*kirchhoff)
 		ind += 1
 	
@@ -39,7 +39,7 @@ def calcElasticEnergy(pdb_ref, ensemble, sel, cutoff, k=1):
 ###################################################################################################################################
 def calcMultiStateEnergy(pdb_sel, ensemble_ref, ensemble, sel, cutoff, k=1, U_0=0):
 	import prody as pd
-	import scipy as sci
+	from scipy import spatial
 	import numpy as np
 
 	# Select specified atoms for analysis
@@ -65,14 +65,14 @@ def calcMultiStateEnergy(pdb_sel, ensemble_ref, ensemble, sel, cutoff, k=1, U_0=
 		kirchhoff = gnm.getKirchhoff()
 		np.fill_diagonal(kirchhoff, 0)
 		kirchhoff = abs(kirchhoff)
-		kirchhoff = sci.spatial.distance.squareform(kirchhoff)
+		kirchhoff = spatial.distance.squareform(kirchhoff)
 
 		# Calculate deformation energies
-		eq_dists = sci.spatial.distance.pdist(coords_ref)
+		eq_dists = spatial.distance.pdist(coords_ref)
 		energies = np.zeros(len(ensemble))
 		j = 0
 		for coords in ensemble.iterCoordsets():
-			dists = sci.spatial.distance.pdist(coords)
+			dists = spatial.distance.pdist(coords)
 			U[i,j] = f_hookean(eq_dists, dists, k*kirchhoff)
 			j += 1
 		U = U + U_0[i]
@@ -125,16 +125,112 @@ def calcMultiStateEnergy(pdb_sel, ensemble_ref, ensemble, sel, cutoff, k=1, U_0=
 
 ###################################################################################################################################
 # Function:		makeLandscape
-# Purpose:		Calculate energy stored in spring deformations from elastic network model according to some reference structure
+# Purpose:		Generate coordinates for landscape structures. Each structure is linked to projections on PC axes for plotting.
 # Arguments:	
 ###################################################################################################################################
+def makeLandscape(pdb, ensemble, sel='name CA', nstep = 100, prefix = 'landscape'):
+	import prody as pd
+	import scipy as sci
+	import numpy as np
+
+	# Initialize ensemble
+	atoms = pdb.select(sel)
+	ensemble.setAtoms(atoms)
+	num_atom = len(ensemble.getAtoms())
+
+	# Calculate PCA modes of transition path
+	pca = pd.PCA('PCA of Transition Path')
+	pca.buildCovariance(ensemble)
+	pca.calcModes()
+
+	# Generate mesh for PC0 and PC1
+	proj0 = pd.calcProjection(ensemble, pca[0])
+	proj1 = pd.calcProjection(ensemble, pca[1])
+	x = np.linspace(min(proj0),max(proj0),nstep)
+	y = np.linspace(min(proj1),max(proj1),nstep)
+	xv, yv = np.meshgrid(x, y, sparse=False, indexing='ij')
+	xv = xv.reshape((xv.size,1))
+	yv = yv.reshape((yv.size,1))
+	proj_land = np.column_stack((xv,yv))
+
+	# Generate landscape structures
+	coords_land = z2xyz(proj_land, ensemble, pca[(0,1)])
+	coords_land = coords_land.reshape((coords_land.shape[0],coords_land.shape[1]/3,3))
+	ensemble_land = pd.Ensemble()
+	ensemble_land.addCoordset(coords_land)
+
+	return((proj_land,ensemble_land))
 
 ###################################################################################################################################
-# Function:		z2xyz
+# Function:		xyz2z
 # Purpose:		Convert a z-score(s) from a principle component to the associated xyz vector. Number of principle components used
 #				for xyz construction must match the number of eigenvectors provided
 # Arguments:	
 ###################################################################################################################################
+# def xyz2z(ensemble, modes, *args, **kwargs):
+# 	proj = calcProjection(ensemble, modes, kwargs.pop('rmsd', True))
+
+
+###################################################################################################################################
+# Function:		z2xyz
+# Purpose:		Convert a z-score(s) from a principle component to the associated xyz vector. Each column of z corresponds to a PC,
+#				and the modes object must only contain information from that number of PCs
+# Arguments:	
+###################################################################################################################################
+def z2xyz(z, ensemble, modes, rmsd = True):
+	import numpy as np
+	import prody as pd
+
+	num_atom = ensemble.numAtoms()
+	# deviations = ensemble.getDeviations()
+	# if deviations.ndim == 3:
+	# 	deviations = deviations.reshape((deviations.shape[0], deviations.shape[1] * 3))
+	# elif deviations.ndim == 2:
+	# 	deviations = deviations.reshape((1, deviations.shape[0] * 3))
+	# else:
+	# 	deviations = deviations.reshape((1, deviations.shape[0]))
+	if rmsd:
+		z = (num_atom**0.5) * z
+	evec = modes.getArray()
+
+	# Make sure shape of evec is properly recognized
+	try:
+		evec.shape[0]
+	except:
+		shape0 = 1
+	else:
+		shape0 = evec.shape[0]
+	try:
+		evec.shape[1]
+	except:
+		shape1 = 1
+	else:
+		shape1 = evec.shape[1]
+	evec = evec.reshape((shape0, shape1))
+
+	# Make sure shape of z is properly recognized
+	try:
+		z.shape[0]
+	except:
+		shape0 = 1
+	else:
+		shape0 = z.shape[0]
+	try:
+		z.shape[1]
+	except:
+		shape1 = 1
+	else:
+		shape1 = z.shape[1]
+	z = z.reshape((shape0, shape1))
+
+	# Calculate coordinates from supplied PCs
+	xyz = ensemble[0].getCoords()
+	xyz = xyz.reshape((1,xyz.shape[0]*xyz.shape[1]))
+	coords = np.dot(evec, z.transpose())
+
+	coords = coords.transpose() + xyz
+
+	return(coords)
 
 ###################################################################################################################################
 # Function:		f_hookean
@@ -145,5 +241,5 @@ def calcMultiStateEnergy(pdb_sel, ensemble_ref, ensemble, sel, cutoff, k=1, U_0=
 ###################################################################################################################################
 def f_hookean(eq_dists, dists, k):
 	import numpy as np
-	import scipy as sci
-	return(np.sum(sci.spatial.distance.squareform(k*(dists-eq_dists)**2)))
+	from scipy import spatial
+	return(np.sum(spatial.distance.squareform(k*(dists-eq_dists)**2)))
